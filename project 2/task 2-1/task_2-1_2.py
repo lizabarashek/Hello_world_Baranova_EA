fdb8e41477260a99c9d43542445d8af36e331206
lizabarashek <vaselisaogneva72@gmail.com>
2026-02-17 12:32:56 +0300

    Add files via upload

---
+++ project 2/task 2-1/task_2-1_2.py
@@ -0,0 +1,10 @@
+# task_2-1_2.py
+
+# Создаем переменную python_version со значением "3.10"
+python_version = "3.10"
+
+# Выводим первую строку
+print("Language: Python")
+
+# Выводим вторую строку с отступом (табуляцией)
+print(f"\tVersion: {python_version}")
