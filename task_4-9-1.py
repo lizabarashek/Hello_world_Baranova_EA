7c689bcf60c11d18fd36bc9f15f25a5c8b2f593c
lizabarashek <vaselisaogneva72@gmail.com>
2026-04-28 15:26:37 +0300

    Add files via upload

---
+++ task_4-9-1.py
@@ -0,0 +1,9 @@
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+
+if num1 > num2:
+    maximum = num1
+else:
+    maximum = num2
+2
+print(f"Максимальное число: {maximum}")
