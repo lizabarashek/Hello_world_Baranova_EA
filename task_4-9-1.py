b29d19ccc7564a6bd88625f6684445ef62d68b03
lizabarashek <vaselisaogneva72@gmail.com>
2026-06-03 21:30:54 +0300

    Delete task_4-9-1.py

--- task_4-9-1.py
+++
@@ -1,9 +0,0 @@
-num1 = float(input("Введите первое число: "))
-num2 = float(input("Введите второе число: "))
-
-if num1 > num2:
-    maximum = num1
-else:
-    maximum = num2
-2
-print(f"Максимальное число: {maximum}")
